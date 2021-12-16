4dc9de86da3f3f41ede5ec1e8834da9b164e478e
Oleksandra Klevets <oklev@googlecloud.corp-partner.google.com>
2021-12-16 21:40:41 +0200

    adjusting Dockerfile for bestpractices

--- sample-app/GraphAnalysis.py
+++ sample-app/GraphAnalysis.py
@@ -2,8 +2,8 @@
 import sys
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import plotly.graph_objects as go
 import random
 import csv
